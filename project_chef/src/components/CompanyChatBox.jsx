671a77e3db206a224fac2ace21a0a6bdb4f5ef4d
Tiago Vinícius <121812462+Tiago-Viniciuss@users.noreply.github.com>
2024-06-21 18:38:45 +0100

    update

--- project_chef/src/components/CompanyChatBox.jsx
+++ project_chef/src/components/CompanyChatBox.jsx
@@ -3,21 +3,20 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs, query, where } from 'firebase/firestore';
 import '../style/CompanyChatBox.css';
 
-const firebaseConfig = { 
-  apiKey: "AIzaSyDLeaVbkontIerNiMt_7SMiX8k3eMeS42o",
+const firebaseConfig = { apiKey: "AIzaSyDLeaVbkontIerNiMt_7SMiX8k3eMeS42o",
   authDomain: "projeto-empregos.firebaseapp.com",
   projectId: "projeto-empregos",
   storageBucket: "projeto-empregos.appspot.com",
   messagingSenderId: "640012785481",
   appId: "1:640012785481:web:8bd5e89d6ca63729666c98",
-  measurementId: "G-H63RS9DHMF" 
-};
+  measurementId: "G-H63RS9DHMF" };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 const CompanyChatBox = () => {
   const [messages, setMessages] = useState([]);
+  const [showPhoto, setShowPhoto] = useState('');
 
   const fetchMessages = async () => {
     try {
@@ -44,31 +43,47 @@ const CompanyChatBox = () => {
     fetchMessages();
   }, []);
 
+  const handlePhotoClick = (photoUrl) => {
+    setShowPhoto(photoUrl);
+  };
+
+  const closePhoto = () => {
+    setShowPhoto(false)
+  }
+
   return (
     <div>
       <h1 id='messsagesTitle'>Minhas Conversas</h1>
       <section id='conversationsContainer'>
-      {messages.length > 0 ? (
-        messages.map(message => (
-          <div key={message.id} className="message">
-            <img src={message && message.CandidatePhoto} alt="" />
-            <p className='messageCandidate'><strong>{message.Name}</strong></p>
-            <p className='messageJobTitle'>{message.JobTitle}</p>
-            <p className='messageContent'>{message.Message}</p>
-            <p className='messageDetails'>Atualmente sou {message.CandidateProfession}</p>
-            <p className='messageDetails'>O meu contacto é <a href={`tel:${message.CandidatePhone}`}>{message.CandidatePhone}</a></p>
-            <p className='messageDetails'>O meu e-mail é <a href={`mailto:${message.CandidateEmail}`}>{message.CandidateEmail}</a></p>
-            <p className='messageDetails'>Eu nasci em {message.CandidateBirthday}</p>
-            <p className='messageDetails'>Falo {message.CandidateLanguage}</p>
-            <p className='messageDetails'>Estudei {message.CandidateEducation}</p>
-            <p className='messageDetails'>Atualmente sou {message.CandidateMarital}</p>
-            <p className='messageDetails'>Possuo {message.CandidateSchool}</p>
-          </div>
-        ))
-      ) : (
-        <p>Nenhuma conversa encontrada.</p>
-      )}
+        {messages.length > 0 ? (
+          messages.map(message => (
+            <div key={message.id} className="message">
+              <img 
+                src={message.CandidatePhoto} 
+                alt="" 
+                onClick={() => handlePhotoClick(message.CandidatePhoto)} 
+                style={{ cursor: 'pointer' }} 
+              />
+              <p className='messageCandidate'><strong>{message.Name}</strong></p>
+              <p className='messageJobTitle'>{message.JobTitle}</p>
+              <p className='messageContent'>{message.Message}</p>
+              <p className='messageDetails'>Atualmente sou {message.CandidateProfession}</p>
+              <p className='messageDetails'>O meu contacto é <a href={`tel:${message.CandidatePhone}`}>{message.CandidatePhone}</a></p>
+              <p className='messageDetails'>O meu e-mail é <a href={`mailto:${message.CandidateEmail}`}>{message.CandidateEmail}</a></p>
+              <p className='messageDetails'>Eu nasci em {message.CandidateBirthday}</p>
+              <p className='messageDetails'>Falo {message.CandidateLanguage}</p>
+              <p className='messageDetails'>Estudei {message.CandidateEducation}</p>
+              <p className='messageDetails'>Atualmente sou {message.CandidateMarital}</p>
+              <p className='messageDetails'>Possuo {message.CandidateSchool}</p>
+            </div>
+          ))
+        ) : (
+          <p>Nenhuma conversa encontrada.</p>
+        )}
       </section>
+      <div id='showPhoto'>
+        {showPhoto && <div><span className='material-symbols-outlined' onClick={closePhoto}>close</span><img src={showPhoto} alt="Candidate" /></div> }
+      </div>
     </div>
   );
 };
