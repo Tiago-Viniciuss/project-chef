40c133d3361c01baf164316a495ec90cd85a309f
Tiago Vinícius <121812462+Tiago-Viniciuss@users.noreply.github.com>
2024-06-28 00:25:08 +0100

    updating again

---
+++ project_chef/src/routes/EditCandidateProfile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const EditCandidateProfile = () => {
+  return (
+    <div>
+        <CandidateOptionsBar/>
+        EditCandidateProfile
+    </div>
+  )
+}
+
+export default EditCandidateProfile
